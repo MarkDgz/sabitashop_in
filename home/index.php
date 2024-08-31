986e13ed16de0292ffb1cdedd4d8c9cb9f44cd1c
Mark Dgz <tnemexico@gmail.com>
2024-08-31 00:22:56 -0300

    Update index.php
    including public php libs

--- home/index.php
+++ home/index.php
@@ -1,3 +1,10 @@
 <?php
-  /* hello world */
+// index.php en la raíz del proyecto
+
+// Incluye el archivo public/index.php
+include 'public/index.php';
+
+// También puedes usar require si deseas que se genere un error fatal si el archivo no se encuentra
+// require 'public/index.php';
+
 ?>
